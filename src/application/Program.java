ee9efa81676a849ea065813394bc257fbc32fb11
capanema <carvalho.capanema@gmail.com>
2019-03-04 21:56:37 -0300

    Exceções e tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,42 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		ChessMatch chessMatch= new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
-	
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getLocalizedMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getLocalizedMessage());
+				sc.nextLine();
+			}
+
+		}
 	}
 
 }
